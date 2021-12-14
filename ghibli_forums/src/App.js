4ab91757ff0bf91b17e0e05fb13f43846bec9ffa
Aaron <imartindizon@gmail.com>
2021-12-14 11:45:31 -0800

    initialized files

---
+++ ghibli_forums/src/App.js
@@ -0,0 +1,15 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { axios } from 'axios';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
